8bd082a63398b3e5ea8e079c1e57919d19760eae
MartinEngen <martin.engen@item.no>
2019-09-15 21:22:03 +0200

    Cleanup TsetlinMachine
    - Removed condition for Type_1 feedback

--- TsetlinMachine.py
+++ TsetlinMachine.py
@@ -1,25 +1,6 @@
 from Clause import generate_clauses
 from utils import get_training_data, eval_xor, type_1_feedback, type_2_feedback
 
-log_conjunctive_clause = False
-def obtain_conjunctive_clauses(clause, sample_x):
-    # Check state of each Tsetlin
-    conjunction = []
-    for index, ta in enumerate(clause.tsetlins):
-        if ta.is_include():
-            # Each sample is considered twice
-            current_sample_value = sample_x[int(index/2)]
-            current_sample_literal_value = ta.literal_value(current_sample_value)
-            conjunction.append(current_sample_literal_value)
-
-            if(ta.designated_literal != int(index / 2)):
-                print("ERROR - TA CONSIDERS THE WRONG VALUE")
-
-            if log_conjunctive_clause:
-                print(f'X{int(index / 2)} - TA: {index} - {ta.should_invert} | {current_sample_value} -> {current_sample_literal_value}')
-
-    return conjunction
-
 
 def calculate_clause_output(clauses: list, sample_x: list) -> list:
     all_clause_outputs = []
@@ -28,8 +9,6 @@ def calculate_clause_output(clauses: list, sample_x: list) -> list:
         all_exclude = 1
 
         for index, x in enumerate(sample_x):
-            print("========================================")
-            print(f'Validating: {x} | Index: X{index}')
             # Get tsetlins in charge of this input
             ta = clause.tsetlins[index * 2]
             ta_negated = clause.tsetlins[(index * 2)+1]
@@ -38,10 +17,7 @@ def calculate_clause_output(clauses: list, sample_x: list) -> list:
             action_include_negated = ta_negated.is_include()
 
             all_exclude = all_exclude and not(action_include == 1 or action_include_negated == 1)
-            print(f'All Exlcude : {all_exclude}')
-            print(f'Action Include : {action_include} | Action Include Neg: {action_include_negated}')
             if (action_include == 1 and x == 0) or (action_include_negated == 1 and x == 1):
-                print("Break")
                 clause_output = 0
                 break
 
@@ -63,27 +39,15 @@ def train_clauses(clauses: list, rounds: int, current_operation: str):
 
         # Get Clause output
         clause_outputs = calculate_clause_output(clauses, sample_x)
-
-        """
-        for clause in clauses:
-
-            clause_conjunction = obtain_conjunctive_clauses(clause, sample_x)
-            clause_output.append(clause_conjunction)
-        """
-        print(clause_outputs)
         sum_votes = sum(clause_outputs)
 
         # sample_y is target for current sample
         if sample_y == 1:
-            # False Negative
-            if sum_votes == 0:
-                for index, res in enumerate(clause_outputs):
-                    current_clause = clauses[index]
-                    type_1_feedback(sample_x, current_clause, res, s)
+            for index, res in enumerate(clause_outputs):
+                current_clause = clauses[index]
+                type_1_feedback(sample_x, current_clause, res, s)
 
             # True Positive
-            else:
-                pass
         # sample_y == 0
         else:
             # False Positives
@@ -97,9 +61,6 @@ def train_clauses(clauses: list, rounds: int, current_operation: str):
                 pass
 
 
-
-
-
 if __name__ == '__main__':
     n_tsetlins_per_clause = 4  # Must be 2x input
     n_clauses = 2  # Must be same amount as input
@@ -110,15 +71,20 @@ if __name__ == '__main__':
     s = 3.9
     t = 1  # Not used, but is indication of how big chance for a feedback to occur based on sum_votes
     n_states_per_decision = 100
-    training_rounds = 100
+    training_rounds = 10000
 
-    clauses = generate_clauses(n_clauses, n_tsetlins_per_clause, n_states_per_decision)
-    train_clauses(clauses, training_rounds, 'XOR')
+    bit_operators_to_learn = ['XOR', 'AND', 'OR']
 
-    print("*** TRANING IS DONE.. ****")
-    clauses[0].show_current_clause()
-    clauses[0].show_states_per_ta()
+    for operator in bit_operators_to_learn:
 
-    clauses[1].show_current_clause()
-    clauses[1].show_states_per_ta()
+        clauses = generate_clauses(n_clauses, n_tsetlins_per_clause, n_states_per_decision)
+        train_clauses(clauses, training_rounds, operator)
+
+        print("*** TRANING IS DONE.. ****")
+        print(f'Operator {operator}')
+        clauses[0].show_current_clause()
+        clauses[0].show_states_per_ta()
+
+        clauses[1].show_current_clause()
+        clauses[1].show_states_per_ta()
 
